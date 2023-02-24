d02e15fa16dd457e257303a38ce6a9de558c3a36
Raza Fatma <126192356+RazaFatma@users.noreply.github.com>
2023-02-24 23:48:49 +0530

    initial Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav-link');
+    const navlist = document.querySelector('.nav-list li');
+
+    menu.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        navList.forEach((link, index) => {
+    });
+        menu.classList.toggle('active');
+    });
+
+
+
+
+
+
+
+
+// const text = [
+// ];
+// const form = document.querySelector('.lorem-more');
+// const amount = document.getElementById('amount');
+// const result = document.querySelector(".lorem-text")
+   
+
+// form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     const value = amount.value;
+//     console.log(typeof value);
+// });
